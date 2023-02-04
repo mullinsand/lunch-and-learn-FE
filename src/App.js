91ade2caba9f08963576dae7a30a1f09af48ce4a
Andrew Mullins <mullinsand15@gmail.com>
2023-02-04 09:07:44 -0700

    country search component successfully built

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import CountrySearch from './CountrySearch';
+import Recipes from './Recipes';
 
 function App() {
+  const [countryName, setCountryName] = useState("");
+  const [countryRecipes, setCountryRecipes] = useState([]);
+  
+
+
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Lunch and Learn</h1>
+      <p>Find Recipes by Country</p>
+      <CountrySearch setCountryName={setCountryName}/>
+      {/* <Recipes /> */}
       </header>
     </div>
   );
