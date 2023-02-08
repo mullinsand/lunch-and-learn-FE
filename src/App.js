cbbab83679b9441119c973aff5bff8ed323d6a05
Andrew Mullins <mullinsand15@gmail.com>
2023-02-08 07:13:29 -0700

    rough navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import CountrySearch from './CountrySearch';
+import Navbar from "./Navigation/Navbar.js";
 
 function App() {
   const [countryName, setCountryName] = useState("");
@@ -8,8 +9,8 @@ function App() {
 
 
   return (
-
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <h1>Lunch and Learn</h1>
         <p>Find Recipes by Country</p>
