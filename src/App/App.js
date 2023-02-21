3cd8ceb989759f59b703433dcce414f9a33d313c
Andrew Mullins <mullinsand15@gmail.com>
2023-02-20 20:47:44 -0700

    useParams hook added to provide country name

--- src/App/App.js
+++ src/App/App.js
@@ -3,19 +3,18 @@ import React, { useState } from 'react';
 import Navbar from "../Navigation/Navbar.js";
 import BaseForm from '../BaseForm/BaseForm';
 import CountryLearning from '../CountryLearning/CountryLearning';
-import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useSearchParams, Switch } from 'react-router-dom';
 
 
 function App() {
   const [countryName, setCountryName] = useState("");
-
   return (
     <BrowserRouter>
       <main>
         <Navbar />
         <Routes>
           <Route path="/" element={<BaseForm/>} />
-          <Route path="/country/:countryName" element={<CountryLearning setCountryName={countryName}/>} />
+          <Route path="/country/:countryName" element={<CountryLearning />} />
         </Routes>
       </main>
     </BrowserRouter>
--- src/CountryLearning/CountryLearning.js
+++ src/CountryLearning/CountryLearning.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import './CountryLearning.css';
+import { BrowserRouter, Routes, Route, useParams, Switch } from 'react-router-dom';
+
 
 const CountryLearning = (props) => {
+  let countryName = useParams();
   return (
     <div>
       You did it!
