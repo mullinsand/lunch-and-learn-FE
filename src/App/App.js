33fde02ee01e38717384bffec587640642f1be16
Andrew Mullins <mullinsand15@gmail.com>
2023-02-26 13:51:18 -0700

    Register component set up and link works in navbar

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import Navbar from "../Navigation/Navbar.js";
 import BaseForm from '../BaseForm/BaseForm';
+import Register from '../Register/Register';
 import CountryLearning from '../CountryLearning/CountryLearning';
 import { Routes, Route } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="country/:countryName" element={<CountryLearning />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/" element={<BaseForm />} />
           {/* <Route path="*" element={<BaseForm/>} /> */}
         </Routes>
---
+++ src/Register/Register.js
@@ -0,0 +1,17 @@
+import './Register.css';
+import React, { useState } from 'react';
+
+
+function Register() {
+
+  return (
+      <main>
+        <h1>
+          Register as a New User!
+        </h1>
+        
+      </main>
+  );
+}
+
+export default Register;
